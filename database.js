e6cdac3baee244f38094431bfedebc4e51898e12
Hili Levitan <hl01052002@gmail.com>
2023-02-26 21:18:33 +0200

    add more routes

--- database.js
+++ database.js
@@ -4,7 +4,8 @@ export const dutiesDBCollection = 'Duties';
 
 async function addNewSoldier(client, newSoldier) {
   newSoldier.duties = [];
-  const soldierInserted = await client.db(dbName).collection(soldiersDBCollection).insertOne(newSoldier);
+  const soldierInserted = await client.db(dbName).collection(soldiersDBCollection)
+    .insertOne(newSoldier);
   return soldierInserted;
 }
 
@@ -14,16 +15,43 @@ async function lookForSoldier(client, specificSoldier) {
 }
 
 async function lookForAllSoldiers(client, specifiedSoldiers) {
-  const result = await client.db(dbName).collection(soldiersDBCollection).find(specifiedSoldiers).toArray();
+  const result = await client.db(dbName).collection(soldiersDBCollection)
+    .find(specifiedSoldiers).toArray();
   return result;
 }
 
-async function createNewDuty(client, specifiedDuty) {
-  specifiedDuty.soldier = [];
-  const dutyInserted = await client.db(dbName).collection(dutiesDBCollection).insertOne(specifiedDuty);
+async function createNewDuty(client, newDuty) {
+  newDuty.soldier = [];
+  const dutyInserted = await client.db(dbName).collection(dutiesDBCollection)
+    .insertOne(newDuty);
   return dutyInserted;
+}
+
+async function lookForAllDuties(client, specifiedDuty) {
+  const result = await client.db(dbName).collection(dutiesDBCollection)
+    .find(specifiedDuty).toArray();
+  return result;
+}
+
+async function lookForDutyById(client, specificDuty) {
+  const result = await client.db(dbName).collection(dutiesDBCollection)
+    .findOne(specificDuty);
+  return result;
+}
+
+async function deleteDutyById(client, specificDuty) {
+  const result = await client.db(dbName).collection(dutiesDBCollection)
+    .deleteOne(specificDuty);
+  return result;
+}
+
+async function updateDuty(client, specificDuty, fieldsToUpdate) {
+  const result = await client.db(dbName).collection(dutiesDBCollection)
+    .updateOne(specificDuty, { $set: fieldsToUpdate });
+  return result;
 }
 
 export {
   addNewSoldier, lookForSoldier, lookForAllSoldiers, createNewDuty,
+  lookForAllDuties, lookForDutyById, deleteDutyById, updateDuty,
 };
--- routes/duties.js
+++ routes/duties.js
@@ -1,4 +1,6 @@
-import { createNewDuty } from '../database.js';
+import {
+  createNewDuty, lookForAllDuties, lookForDutyById, deleteDutyById, updateDuty,
+} from '../database.js';
 import { client } from '../connections.js';
 
 const newDutySchema = {
@@ -23,6 +25,80 @@ export default function dutyRouter(app, opts, done) {
     dutyInserted._id = Math.floor(Math.random() * 1000000);
     await createNewDuty(client, dutyInserted);
     res.status(201).send('duty created');
+  });
+
+  app.get('/', async (req, res) => {
+    const dutiesQuery = req.query;
+    const objectSent = {};
+    if (dutiesQuery.name) {
+      objectSent.name = dutiesQuery.name;
+    }
+    if (dutiesQuery.location) {
+      objectSent.location = Number(dutiesQuery.location);
+    }
+    if (dutiesQuery.time) {
+      objectSent.time = dutiesQuery.time;
+    }
+    if (dutiesQuery.constraints) {
+      objectSent.constraints = dutiesQuery.constraints;
+    }
+    if (dutiesQuery.soldiersRequired) {
+      objectSent.soldiersRequired = Number(dutiesQuery.soldiersRequired);
+    }
+    if (dutiesQuery.value) {
+      objectSent.value = dutiesQuery.value;
+    }
+    const searchResult = await lookForAllDuties(client, objectSent);
+    if (searchResult) {
+      res.send(searchResult);
+    } else {
+      res.send('found nothing');
+    }
+  });
+
+  app.get('/:id', async (req, res) => {
+    const dutyId = req.params.id;
+    const searchResponse = await lookForDutyById(
+      client,
+      { _id: Number(dutyId) },
+    );
+    if (searchResponse) {
+      res.status(200).send('duty found');
+    } else {
+      res.status(400).send('duty not found');
+    }
+  });
+
+  app.delete('/:id', async (req, res) => {
+    const idForDelete = req.params.id;
+    const searchResponse = await lookForDutyById(
+      client,
+      { _id: Number(idForDelete) },
+    );
+    if (searchResponse) {
+      await deleteDutyById(
+        client,
+        { _id: Number(idForDelete) },
+      );
+      res.status(200).send('duty deleted');
+    } else {
+      res.status(400).send('duty doesnt exist');
+    }
+  });
+
+  app.patch('/:id', { newDutySchema }, async (req, res) => {
+    const dutyToUpdate = req.params.id;
+    const updatesToDo = req.body;
+    const updateResult = await updateDuty(
+      client,
+      { _id: Number(dutyToUpdate) },
+      updatesToDo,
+    );
+    if (updateResult.modifiedCount === 1) {
+      res.send('duty updated');
+    } else {
+      res.send('not updated');
+    }
   });
   done();
 }
