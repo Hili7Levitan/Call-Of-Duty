43a925fe73ab98d305df5b7bd6105a6965356e3e
Hili Levitan <hl01052002@gmail.com>
2023-06-25 17:34:44 +0300

    Add helmet and swagger

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import Fastify from 'fastify';
+import fastifyHelmet from '@fastify/helmet';
+import swaggerUI from '@fastify/swagger-ui';
+import swagger from '@fastify/swagger';
 import soldierRouter from './routes/soldiers.js';
 import dutyRouter from './routes/duties.js';
 import healthRouter from './routes/health.js';
@@ -11,9 +14,29 @@ const app = Fastify({
   logger: logOptions,
 });
 
+app.register(fastifyHelmet);
+app.register(swagger, {
+  swagger: {
+    info: {
+      title: 'Call Of Duty API swagger',
+      description: 'API documentation for Call Of Duty project',
+      version: '1.0.0',
+    },
+    host: 'http://localhost:3000/',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+  },
+  exposeRoute: true,
+});
+
+app.register(swaggerUI, { prefix: '/api-docs' });
 app.register(soldierRouter, { prefix: '/soldiers' });
 app.register(dutyRouter, { prefix: '/duties' });
 app.register(healthRouter, { prefix: '/health' });
 app.register(justiceBoardRouter, { prefix: '/justice-board' });
+
+await app.ready();
+app.swagger();
 
 export default app;
